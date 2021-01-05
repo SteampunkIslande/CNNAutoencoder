ebca3c586f5fbd0a336e5932cad643990530adfb
SteampunkIslande <charles.monod-broca@neuf.fr>
2021-01-05 15:15:48 +0100

    Created render_checkpoint.py
    This script allows one to render image out of any checkpoint during the training of an autoencoder

---
+++ render_checkpoint.py
@@ -0,0 +1,32 @@
+import torch
+from unet import UNet
+import torchvision
+
+import argparse
+
+from test_model import renderImage
+
+toPIL = torchvision.transforms.ToPILImage()
+
+def renderCheckpoint(checkpoint_path,input_image,exposure_correction,tile_size):
+    model = UNet()
+    checkpoint = torch.load(checkpoint_path,map_location=lambda storage, loc: storage)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    model.eval()
+    npImg = renderImage(model,input_image,exposure_correction,tile_size)
+    return npImg
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("checkpoint_file_name", help="Path to the checkpoint to test")
+    parser.add_argument("image",help="Path to the image to render using checkpoint's model")
+    parser.add_argument("exposure_correction",help="Exposure correction ratio to apply while rendering this image",type=int)
+    parser.add_argument("-o","--output",help="The path to save the render to",default="result.jpg")
+    parser.add_argument("--tile_size",help="How big the tile for rendering",type=int,default=256)
+    args = parser.parse_args()
+
+    image = toPIL(renderCheckpoint(args.checkpoint_file_name,args.image,args.exposure_correction,args.tile_size))
+    image.save(args.output)
+
+
